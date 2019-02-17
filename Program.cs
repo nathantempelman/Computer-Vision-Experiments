ae735149cc2bc21d072afdf97f670df404d3292a
Nathan Tempelman <nathantempelman@gmail.com>
2019-02-17 15:41:04 -0800

    Reads from webcam and detects eyes realtime

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+
+namespace FuxingWIthCamerasNoWeb
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //String win1 = "Test Window"; //The name of the window
+            //CvInvoke.NamedWindow(win1); //Create the window using the specific name
+
+            //Mat img = new Mat(200, 400, DepthType.Cv8U, 3); //Create a 3 channel image of 400x200
+            //img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
+
+            ////Draw "Hello, world." on the image using the specific font
+            //CvInvoke.PutText(
+            //   img,
+            //   "Hello, world",
+            //   new System.Drawing.Point(10, 80),
+            //   FontFace.HersheyComplex,
+            //   1.0,
+            //   new Bgr(0, 255, 0).MCvScalar);
+
+
+            //CvInvoke.Imshow(win1, img); //Show the image
+            //CvInvoke.WaitKey(0);  //Wait for the key pressing event
+            //CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
+
+            //CvInvoke.CalibrateCamera();
+            //ImageViewer viewer = new ImageViewer(); //create an image viewer
+            //Capture capture = new Capture(); //create a camera captue
+            //Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+            //{  //run this until application closed (close button click on image viewer)
+            //    viewer.Image = capture.QueryFrame(); //draw the image obtained from camera
+            //});
+            //viewer.ShowDialog(); //show the image viewer
+
+            //VideoCapture vc = new VideoCapture()
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new VidCapper());
+        }
+    }
+}
---
+++ VidCapper.cs
@@ -0,0 +1,91 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace FuxingWIthCamerasNoWeb
+{
+    public partial class VidCapper : Form
+    {
+        VideoCapture _capture;
+        private Mat _frame;
+
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                Image<Gray, byte> grayframe = _frame.ToImage<Gray, byte>();
+
+                //IImage image1 = new UMat()
+
+                long detectionTime;
+                List<Rectangle> faces = new List<Rectangle>();
+                List<Rectangle> eyes = new List<Rectangle>();
+
+                DetectFace.Detect(
+                  _frame, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+                  faces, eyes,
+                  out detectionTime);
+
+                foreach (Rectangle face in faces)
+                    CvInvoke.Rectangle(_frame, face, new Bgr(Color.Red).MCvScalar, 2);
+                foreach (Rectangle eye in eyes)
+                    CvInvoke.Rectangle(_frame, eye, new Bgr(Color.Blue).MCvScalar, 2);
+
+                //display the image 
+                //using (InputArray iaImage = image.GetInputArray())
+                //    ImageViewer.Show(image, String.Format(
+                //       "Completed face and eye detection using {0} in {1} milliseconds",
+                //       (iaImage.Kind == InputArray.Type.CudaGpuMat && CudaInvoke.HasCuda) ? "CUDA" :
+                //       (iaImage.IsUMat && CvInvoke.UseOpenCL) ? "OpenCL"
+                //       : "CPU",
+                //       detectionTime));
+
+
+              //  pictureBox1.Image = _frame.Bitmap;
+
+                pictureBox1.Image = _frame.Bitmap;
+            }
+        }
+
+        public VidCapper()
+        {
+            InitializeComponent();
+
+            _capture = new VideoCapture(0);
+
+
+            _capture.ImageGrabbed += ProcessFrame;
+            _frame = new Mat();
+            if (_capture != null)
+            {
+                try
+                {
+                    _capture.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
